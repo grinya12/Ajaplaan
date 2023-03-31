e8abe46d0c11e0757b9b67ab5a0ef9a7980834b0
Grigori Gritsenko <grissha2002@gmail.com>
2023-03-31 10:17:07 +0300

    ajaplaan new

--- Ajaplaan/Ajaplaan/AjaplaanPage.xaml.cs
+++ Ajaplaan/Ajaplaan/AjaplaanPage.xaml.cs
@@ -25,72 +25,72 @@ namespace Ajaplaan
             if (_triggerTime.Hours.ToString() == "4")
             {
                 LabelInfo.Text = "Get up";
-                imageforpicker.Source = "1getup.png";
+                imageforpicker.Source = "getup.png";
             }
             else if (_triggerTime.Hours.ToString() == "5")
             {
                 LabelInfo.Text = "Brush your teeth";
-                imageforpicker.Source = "2teeth.png";
+                imageforpicker.Source = "teeth.png";
             }
             else if (_triggerTime.Hours.ToString() == "6")
             {
                 LabelInfo.Text = "Have breakfast";
-                imageforpicker.Source = "3breakfast.png";
+                imageforpicker.Source = "breakfast.png";
             }
             else if (_triggerTime.Hours.ToString() == "7")
             {
                 LabelInfo.Text = "Take a shower";
-                imageforpicker.Source = "4shower.png";
+                imageforpicker.Source = "shower.png";
             }
             else if (_triggerTime.Hours.ToString() == "8")
             {
                 LabelInfo.Text = "Get dressed";
-                imageforpicker.Source = "5dressed.png";
+                imageforpicker.Source = "dressed.png";
             }
             else if (_triggerTime.Hours.ToString() == "9")
             {
                 LabelInfo.Text = "Go to school";
-                imageforpicker.Source = "6school.png";
+                imageforpicker.Source = "school.png";
             }
             else if (_triggerTime.Hours.ToString() == "13")
             {
                 LabelInfo.Text = "Study English";
-                imageforpicker.Source = "7study.png";
+                imageforpicker.Source = "study.png";
             }
             else if (_triggerTime.Hours.ToString() == "15")
             {
                 LabelInfo.Text = "Have a lunch";
-                imageforpicker.Source = "8lunch.png";
+                imageforpicker.Source = "lunch.png";
             }
             else if (_triggerTime.Hours.ToString() == "16")
             {
                 LabelInfo.Text = "Exercise";
-                imageforpicker.Source = "9exercise.png";
+                imageforpicker.Source = "exercise.png";
             }
             else if (_triggerTime.Hours.ToString() == "17")
             {
                 LabelInfo.Text = "Do your homework";
-                imageforpicker.Source = "10homework.png";
+                imageforpicker.Source = "homework.png";
             }
             else if (_triggerTime.Hours.ToString() == "19")
             {
                 LabelInfo.Text = "Cook dinner";
-                imageforpicker.Source = "11dinner.png";
+                imageforpicker.Source = "dinner.png";
             }
             else if (_triggerTime.Hours.ToString() == "20")
             {
                 LabelInfo.Text = "Watch TV";
-                imageforpicker.Source = "12tv.png";
+                imageforpicker.Source = "tv.png";
             }
             else if (_triggerTime.Hours.ToString() == "21")
             {
                 LabelInfo.Text = "Take a bath";
-                imageforpicker.Source = "13bath.png";
+                imageforpicker.Source = "bath.png";
             }
             else if (_triggerTime.Hours.ToString() == "22")
             {
                 LabelInfo.Text = "Go to bed";
-                imageforpicker.Source = "14ded.png";
+                imageforpicker.Source = "bed.png";
             }
 
             ButtonTagasi.Clicked += ButtonTagasi_Clicked;
